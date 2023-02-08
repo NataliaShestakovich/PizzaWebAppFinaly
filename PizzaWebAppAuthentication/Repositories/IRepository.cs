0db3e07177ec37a5f7ba4a9972ab7018319bf947
Natallia <nata_pinsk@tut.by>
2023-02-08 21:07:04 +0300

    Добавлен контролер менеджмента юзеров, добавлен интерфейс репозитория и юзерРепозиторий(без реализации)

---
+++ PizzaWebAppAuthentication/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaWebAppAuthentication.Repositories
+{
+    public interface IRepository <T1, T2> where T1 : class 
+                                          where T2: struct
+    {
+        void Update(T1 objectT);
+        void Update(T2 objectT);
+
+        void Delete(T1 objectT);
+        void Delete(T2 objectT);
+
+        T1? Get(Guid id);
+
+        List<T1> GetList();
+
+    }
+}
---
+++ PizzaWebAppAuthentication/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using PizzaWebAppAuthentication.Models.AppModels;
+
+namespace PizzaWebAppAuthentication.Repositories
+{
+    public class UserRepository : IRepository<ApplicationUser, Guid>
+    {
+        public void Delete(ApplicationUser objectT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid objectT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser? Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ApplicationUser> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ApplicationUser objectT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid objectT)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
