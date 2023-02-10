7754afdedd3b56bf8b5150bd3348c8edf778bfce
Natallia <nata_pinsk@tut.by>
2023-02-10 11:01:35 +0300

    Добавлена настройка серилога в программе

--- PizzaWebAppAuthentication/Program.cs
+++ PizzaWebAppAuthentication/Program.cs
@@ -6,6 +6,8 @@ using PizzaWebAppAuthentication.Data.Seed;
 using PizzaWebAppAuthentication.Models.AppModels;
 using PizzaWebAppAuthentication.Services.RoleManagementService;
 using PizzaWebAppAuthentication.Services.Sendgrid;
+using Serilog;
+using Serilog.Events;
 
 namespace PizzaWebAppAuthentication
 {
@@ -13,66 +15,90 @@ namespace PizzaWebAppAuthentication
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(@"D:\Users\PizzaLog folder\logs\data.log", LogEventLevel.Information)
+                .CreateLogger();
             
-            builder.Services.AddControllersWithViews();
+            Log.Information("Starting up!");
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-           
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity <ApplicationUser> (options =>
-            { options.SignIn.RequireConfirmedAccount = false;
-              options.Lockout.AllowedForNewUsers = true;
-              options.Lockout.MaxFailedAccessAttempts = 3;
-              options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
-              options.User.RequireUniqueEmail = true;
-            })
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            
-            builder.Services.AddTransient<IEmailSender, EmailSender>();
-
-            builder.Services.AddTransient<CreateDefaultUserService>();
-
-            builder.Services.AddScoped<RoleManagementService>(); //Посмотреть после если будет интерфейс
-
-
-            var app = builder.Build();
-
-            await Startup.InitializeIdentities(app.Services);
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseMigrationsEndPoint();
+                Log.Information("Starting web application");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog();
+
+                // Add services to the container.
+
+                builder.Services.AddControllersWithViews();
+
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(connectionString));
+
+                builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+                builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Lockout.AllowedForNewUsers = true;
+                    options.Lockout.MaxFailedAccessAttempts = 3;
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                    options.User.RequireUniqueEmail = true;
+                })
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+                builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+                builder.Services.AddTransient<CreateDefaultUserService>();
+
+                builder.Services.AddScoped<RoleManagementService>(); //Посмотреть после если будет интерфейс
+
+
+                var app = builder.Build();
+
+                await Startup.InitializeIdentities(app.Services);
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseMigrationsEndPoint();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                app.MapRazorPages();
+
+                app.Run();
+
             }
-            else
+            catch (Exception ex)
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                Log.Fatal(ex, "Application terminated unexpectedly");
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
-            app.Run();
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
