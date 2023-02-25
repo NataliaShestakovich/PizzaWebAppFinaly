36445973cf4080c0cfb98ae9eaeb3fcebeaf18a9
Volha Shastakovich <Volha_Shastakovich@epam.com>
2023-02-25 22:16:24 +0300

    Сделала миграцию для новых таблиц Пиццы и ингредиентов

--- PizzaWebAppAuthentication/Data/Migrations/20230225172342_Pizzas.Designer.cs
+++ PizzaWebAppAuthentication/Data/Migrations/20230225172342_Pizzas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaWebAppAuthentication.Data;
 
@@ -11,9 +12,11 @@ using PizzaWebAppAuthentication.Data;
 namespace PizzaWebAppAuthentication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230225172342_Pizzas")]
+    partial class Pizzas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
