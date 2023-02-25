65f271f208e605bcd8b28ed23eefd09f4a9b15fd
Volha Shastakovich <Volha_Shastakovich@epam.com>
2023-02-25 22:12:49 +0300

    Добавила модели пиццы и ингредиента

---
+++ PizzaWebAppAuthentication/Models/AppModels/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaWebAppAuthentication.Models.AppModels
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int PricePerKg { get; set; }
+    }
+}
---
+++ PizzaWebAppAuthentication/Models/AppModels/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaWebAppAuthentication.Models.AppModels
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Precision(25, 5)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
