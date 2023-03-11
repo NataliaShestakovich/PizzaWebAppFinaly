f3738b8afc67508c9989e6c89ffcdefeab630671
Natallia <nata_pinsk@tut.by>
2023-03-11 22:14:34 +0300

    Добавлен метод Создания Post пиццы администратором

--- PizzaWebAppAuthentication/Areas/Admin/Controllers/PizzaController.cs
+++ PizzaWebAppAuthentication/Areas/Admin/Controllers/PizzaController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using PizzaWebAppAuthentication.Data;
+using PizzaWebAppAuthentication.Models.AppModels;
 using PizzaWebAppAuthentication.Models.ViewModels.CartViewModeles;
 using PizzaWebAppAuthentication.Repositories;
 
@@ -22,15 +24,54 @@ namespace PizzaWebAppAuthentication.Areas.Admin.Controllers
             _webHostEnvironment = webHostEnvironment;
         }
 
-        public IActionResult Create()
-        {
-            
-            return View();
-        }
-
         public async Task<IActionResult> Index()
         {
             return View(await _pizzaRepository.GetPizzas());
         }
+        
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Pizza pizza)
+        {
+            if (ModelState.IsValid)
+            {
+                var name = await _contextDb.Pizzas.FirstOrDefaultAsync(p => p.Name == pizza.Name);
+                if (name != null) 
+                {
+                    ModelState.AddModelError("", $"Pizza {pizza.Name} already exists.");
+                    return View(pizza);
+                }
+
+                if (pizza.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string imageName = Guid.NewGuid().ToString() + "_" + pizza.ImageUpload.FileName;
+
+                    string filePath = Path.Combine(uploadsDir, imageName);
+
+                    FileStream fileStream = new(filePath, FileMode.Create);
+                    await pizza.ImageUpload.CopyToAsync(fileStream);
+                    fileStream.Close();
+
+                    pizza.ImagePath = imageName;
+
+                }
+
+                _contextDb.Add(pizza);
+                await _contextDb.SaveChangesAsync();
+
+                TempData["Success"] = $"Pizza {pizza.Name} has been created";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(pizza);
+        }
+
     }
 }
