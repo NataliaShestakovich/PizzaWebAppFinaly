fadfd1c2697365a514fff5d7f265d03e429de0d8
Natallia <nata_pinsk@tut.by>
2023-03-14 15:30:55 +0300

    Миграции и Инициализация бд без description

--- PizzaWebAppAuthentication/Migrations/20230314105900_Initialization.Designer.cs
+++ PizzaWebAppAuthentication/Migrations/20230314105900_Initialization.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaWebAppAuthentication.Data;
 
@@ -11,9 +12,11 @@ using PizzaWebAppAuthentication.Data;
 namespace PizzaWebAppAuthentication.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230314105900_Initialization")]
+    partial class Initialization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -357,6 +360,9 @@ namespace PizzaWebAppAuthentication.Migrations
                         .HasColumnType("uniqueidentifier");
 
                     b.Property<string>("Composition")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Description")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ImagePath")
--- PizzaWebAppAuthentication/Migrations/20230314115850_Without Description.Designer.cs
+++ PizzaWebAppAuthentication/Migrations/20230314115850_Without Description.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaWebAppAuthentication.Data;
 
@@ -11,9 +12,11 @@ using PizzaWebAppAuthentication.Data;
 namespace PizzaWebAppAuthentication.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230314115850_Without Description")]
+    partial class WithoutDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
