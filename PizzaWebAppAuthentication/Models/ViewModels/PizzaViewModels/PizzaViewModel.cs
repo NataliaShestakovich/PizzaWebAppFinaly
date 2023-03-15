dff43ae17b76265985c3498493b6281a921691c8
Natallia <nata_pinsk@tut.by>
2023-03-15 09:40:40 +0300

    Перенесла в папку ПиццаВьюМодел

---
+++ PizzaWebAppAuthentication/Models/ViewModels/PizzaViewModels/PizzaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PizzaWebAppAuthentication.Models.AppModels;
+
+namespace PizzaWebAppAuthentication.Models.ViewModels.PizzaViewModels
+{
+    public class PizzaViewModel
+    {
+        public string Base { get; set; }
+        public double Size { get; set; }
+        public List<string> Ingredients { get; set; }
+    }
+}
