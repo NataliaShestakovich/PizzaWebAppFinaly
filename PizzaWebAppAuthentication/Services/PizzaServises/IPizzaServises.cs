0698d73ec0a91503886ff1ace931bd1a67631233
Natallia <nata_pinsk@tut.by>
2023-03-15 09:38:30 +0300

    Создала сервисы по работе с данными о пицце которые обращаются к репозиторию при создании пиццы стандартной

---
+++ PizzaWebAppAuthentication/Services/PizzaServises/IPizzaServises.cs
@@ -0,0 +1,19 @@
+﻿using PizzaWebAppAuthentication.Models.AppModels;
+
+namespace PizzaWebAppAuthentication.Services.PizzaServises
+{
+    public interface IPizzaServices
+    {
+        Task<IEnumerable<Pizza>> GetStandartPizzasAsync();
+
+        IEnumerable<string> GetIngredients();
+
+        PizzaBase GetPizzaBaseByName(string baseName);
+
+        Size GetSizeByDiameter(double sizeName);
+
+        Ingredient GetIngredientByName(string ingredientName);
+
+        Task<string> AddPizzaToDataBaseAsync(Pizza pizza);
+    }
+}
---
+++ PizzaWebAppAuthentication/Services/PizzaServises/PizzaServices.cs
@@ -0,0 +1,53 @@
+﻿using PizzaWebAppAuthentication.Data;
+using PizzaWebAppAuthentication.Models.AppModels;
+using PizzaWebAppAuthentication.Repositories;
+
+namespace PizzaWebAppAuthentication.Services.PizzaServises
+{
+    public class PizzaServices : IPizzaServices
+    {
+        private readonly IPizzaRepository _pizzaRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PizzaServices( IPizzaRepository pizzaRepository,
+                              IWebHostEnvironment webHostEnvironment)
+        {
+            _pizzaRepository = pizzaRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IEnumerable<Pizza>> GetStandartPizzasAsync()
+        {
+            var pizzas = await _pizzaRepository.GetStandartPizzasAsync();
+
+            return pizzas;
+        }
+
+        public IEnumerable<string> GetIngredients()
+        {
+            var ingredients = _pizzaRepository.GetIngredients();
+
+            return ingredients;
+        }
+
+        public PizzaBase GetPizzaBaseByName(string baseName)
+        {
+            return (_pizzaRepository.GetPizzaBaseByName(baseName));
+        }
+
+       public Size GetSizeByDiameter(double sizeName)
+        {
+            return (_pizzaRepository.GetSizeByDiameter(sizeName));
+        }
+
+        public Ingredient GetIngredientByName(string ingredientName)
+        {
+            return (_pizzaRepository.GetIngredientByName(ingredientName));
+        }
+
+        public Task<string> AddPizzaToDataBaseAsync(Pizza pizza)
+        {
+            return (_pizzaRepository.AddPizzaToDataBaseAsync(pizza));
+        }
+    }
+}
