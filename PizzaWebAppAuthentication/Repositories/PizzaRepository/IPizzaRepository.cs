30715128cd0b67d392bb2699a9c53b946a4857cd
Natallia <nata_pinsk@tut.by>
2023-03-17 08:53:19 +0300

    Исправеления в репозитории

---
+++ PizzaWebAppAuthentication/Repositories/PizzaRepository/IPizzaRepository.cs
@@ -0,0 +1,26 @@
+﻿using PizzaWebAppAuthentication.Models.AppModels;
+
+namespace PizzaWebAppAuthentication.Repositories.PizzaRepository;
+
+public interface IPizzaRepository
+{
+    Task<IEnumerable<Pizza>> GetStandartPizzasAsync();
+
+    Task<Pizza> GetStandartPizzaByIdAsync(Guid id);
+
+    IEnumerable<string> GetIngredients();
+
+    PizzaBase GetPizzaBaseByName(string baseName);
+
+    Size GetSizeByDiameter(double sizeName);
+
+    Ingredient GetIngredientByName(string ingredientName);
+
+    List<Pizza> GetPizzasByName(string name);
+
+    Task<string> AddPizzaToDataBaseAsync(Pizza pizza);
+
+    Task<string> UpdatePizzaInDataBaseAsync(Pizza pizza);
+
+    Task<string> DeletePizzaFromDataBaseAsync(Pizza pizza);
+}
---
+++ PizzaWebAppAuthentication/Repositories/PizzaRepository/PizzaRepository.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaWebAppAuthentication.Data;
+using PizzaWebAppAuthentication.Models.AppModels;
+using System.Collections;
+
+namespace PizzaWebAppAuthentication.Repositories.PizzaRepository
+{
+    public class PizzaRepository : IPizzaRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public PizzaRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Pizza>> GetStandartPizzasAsync()
+        {
+            var pizzas = await _context.Pizzas
+                .Where(c => c.Standart == true)
+                .Include(c => c.Ingredients)
+                .Include(c => c.PizzaBase)
+                .Include(c => c.Size)
+                .ToListAsync();
+
+            return pizzas;
+        }
+
+        public async Task<Pizza> GetStandartPizzaByIdAsync(Guid id)
+        {
+            var pizza = await _context.Pizzas
+                .Where(c => c.Standart == true)
+                .Where(p => p.Id == id)
+                .Include(c => c.Ingredients)
+                .Include(c => c.PizzaBase)
+                .Include(c => c.Size)
+                .FirstOrDefaultAsync();
+
+            return pizza;
+        }
+
+        public IEnumerable<string> GetIngredients()
+        {
+            return _context.Ingredients.Select(x => x.Name).ToList();
+        }
+
+        public PizzaBase GetPizzaBaseByName(string baseName)
+        {
+            return _context.Bases.Where(c => c.Name == baseName).FirstOrDefault();
+        }
+
+        public Size GetSizeByDiameter(double sizeName)
+        {
+            return _context.Sizes.Where(c => c.Diameter == sizeName).FirstOrDefault();
+        }
+
+        public Ingredient GetIngredientByName(string ingredientName)
+        {
+            return _context.Ingredients.Where(c => c.Name == ingredientName).FirstOrDefault();
+        }
+
+        public List<Pizza> GetPizzasByName(string name)
+        {
+            var pizzas = _context.Pizzas.Where(p => p.Name == name)
+                                        .ToList();
+            return pizzas;
+        }
+
+        public async Task<string> AddPizzaToDataBaseAsync(Pizza pizza)
+        {
+            _context.Add(pizza);
+
+            await _context.SaveChangesAsync();
+
+            return $"Pizza {pizza.Name} has been created";
+        }
+
+        public async Task<string> UpdatePizzaInDataBaseAsync(Pizza pizza)
+        {
+            _context.Update(pizza);
+
+            await _context.SaveChangesAsync();
+
+            return $"Pizza {pizza.Name} has been edited";
+        }
+
+        public async Task<string> DeletePizzaFromDataBaseAsync(Pizza pizza)
+        {
+            _context.Pizzas.Remove(pizza);
+            await _context.SaveChangesAsync();
+
+            return $"Pizza {pizza.Name} has been deleted";
+        }
+
+
+
+
+    }
+}
