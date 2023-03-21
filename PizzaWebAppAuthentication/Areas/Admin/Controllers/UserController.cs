dd24caef9764a010efab7d9fc998b7e596136746
Natallia <nata_pinsk@tut.by>
2023-03-22 00:12:53 +0300

    Добавлены трай кетч для методов контроллера User у админа

--- PizzaWebAppAuthentication/Areas/Admin/Controllers/UserController.cs
+++ PizzaWebAppAuthentication/Areas/Admin/Controllers/UserController.cs
@@ -12,65 +12,85 @@ namespace PizzaWebAppAuthentication.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManagementService _userService;
-        public UserController(UserManagementService userService)
+        private readonly ILogger<UserController> _logger;
+        public UserController(UserManagementService userService, ILogger<UserController> logger)
         {
             _userService = userService;
+            _logger = logger;   
         }
 
         public async Task<IActionResult> Index(UsersListViewModel usersListViewModel)
         {
-            List<ApplicationUser> userList = new();
-
-            if (usersListViewModel != null)
+            try
             {
-                if (!string.IsNullOrEmpty(usersListViewModel.SelectedRole))
+                List<ApplicationUser> userList = new();
+
+                if (usersListViewModel != null)
                 {
-                    userList = await _userService.GetUsersByRoleAsync(usersListViewModel.SelectedRole);
+                    if (!string.IsNullOrEmpty(usersListViewModel.SelectedRole))
+                    {
+                        userList = await _userService.GetUsersByRoleAsync(usersListViewModel.SelectedRole);
+                    }
+                    else
+                    {
+                        userList = _userService.GetUsers();
+                    }
+                    if (!string.IsNullOrEmpty(usersListViewModel.SelectedEmail))
+                    {
+                        userList = userList.Where(e => e.Email == usersListViewModel.SelectedEmail).ToList();
+                    }
+                    if (!string.IsNullOrEmpty(usersListViewModel.SelectedFirstName))
+                    {
+                        userList = userList.Where(e => e.FirstName == usersListViewModel.SelectedFirstName).ToList();
+                    }
                 }
                 else
                 {
                     userList = _userService.GetUsers();
                 }
-                if (!string.IsNullOrEmpty(usersListViewModel.SelectedEmail))
-                {
-                    userList = userList.Where(e => e.Email == usersListViewModel.SelectedEmail).ToList();
-                }
-                if (!string.IsNullOrEmpty(usersListViewModel.SelectedFirstName))
-                {
-                    userList = userList.Where(e => e.FirstName == usersListViewModel.SelectedFirstName).ToList();
-                }
-            }
-            else
-            {
-                userList = _userService.GetUsers();
-            }
 
-            var _usersListViewModel = new UsersListViewModel
-            {
-                Users = userList.ToList(),
-                Role = _userService.GetSelectListRoles(),
-                FirstName = _userService.GetSelectListFirstName(),
-                Email = _userService.GetSelectListEmail(),
-            };
+                var _usersListViewModel = new UsersListViewModel
+                {
+                    Users = userList.ToList(),
+                    Role = _userService.GetSelectListRoles(),
+                    FirstName = _userService.GetSelectListFirstName(),
+                    Email = _userService.GetSelectListEmail(),
+                };
 
-            return View(_usersListViewModel);
+                return View(_usersListViewModel);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode(500);
+            }
+            
         }
 
         public async Task<IActionResult> Details(Guid id)
         {
-            var user = await _userService.GetUserByIDAsync(id);
-
-            var userData = new UserDataViewModel
+            try
             {
-                Id = id,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Email = user.Email,
-                Phone = user?.PhoneNumber ?? string.Empty,
-                Role = await _userService.GetRoleByUserAsync(user),
-                DateLastOrder = DateTime.Today.AddDays(new Random().Next(-5, -1))
-            };
-            return View(userData);
+                var user = await _userService.GetUserByIDAsync(id);
+
+                var userData = new UserDataViewModel
+                {
+                    Id = id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Phone = user?.PhoneNumber ?? string.Empty,
+                    Role = await _userService.GetRoleByUserAsync(user),
+                    DateLastOrder = DateTime.Today.AddDays(new Random().Next(-5, -1))
+                };
+                return View(userData);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode(500);
+            }
+            
         }
     }
 }
