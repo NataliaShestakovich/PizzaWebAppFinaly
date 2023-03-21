4f3f249cbbc9979336921d9098e1e654c85c08d8
Natallia <nata_pinsk@tut.by>
2023-03-21 16:53:35 +0300

    Создан дублика ингредиентРепозитория для исправления кода

---
+++ PizzaWebAppAuthentication/Repositories/IngredientRepository/FullIngredientRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace PizzaWebAppAuthentication.Repositories.IngredientRepository
+{
+    public class FullIngredientRepository
+    {
+        public class IngredientRepository : RepositoryBase<Ingredient, int>, IIngredientRepository
+        {
+            public IngredientRepository(ApplicationDbContext dbContext) : base(dbContext)
+            {
+            }
+                       
+            public async Task<IEnumerable<Ingredient>> GetIngredientsByName(string name)
+            {
+                return await _dbContext.Set<Ingredient>().Where(i => i.Name == name).ToListAsync();
+            }
+            
+
+            public async Task<bool> IngredientExistsAsync(string name, int id)
+            {
+                return await _dbContext.Set<Ingredient>().AnyAsync(i => i.Name == name && i.Id != id);
+            }
+
+
+            
+
+        }
+    }
+}
